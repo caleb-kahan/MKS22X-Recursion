86acde2586c6df8ed87212881513dd49f5e8b392
Caleb Kahan <ckahan00@stuy.edu>
2019-02-05 17:30:29 -0500

    deleted comments and fibh

--- Recursion.java
+++ Recursion.java
@@ -1,12 +1,6 @@
 import java.util.ArrayList;
 public class Recursion{
-    /*You may write additional private methods */
 
-    /*Recursively find the sqrt using Newton's approximation
-     *tolerance is the allowed percent error the squared answer is away from n.
-     *precondition: n is non-negative
-
-    */
     public static double sqrt(double n, double tolerance){
       return sqrtHelp(n,tolerance,n/2);
     }
@@ -16,12 +10,6 @@ public class Recursion{
 
     }
 
-    /*Recursively find the n'th fibbonaci number in linear time
-     *fib(0) = 0
-     *fib(1) = 1
-     *fib(5) = 5
-     *precondition: n is non-negative
-     */
     public static int fib(int n){
         return fib(n,0,1);
     }
@@ -30,10 +18,8 @@ public class Recursion{
         return fib(n-1,first, zero+first);
         //Logic: From to get N to 0, there will be n runs.
         //That means the 0th number will be updated n times.
-        //0+n>Nth number of fibonacci sequence. 
+        //0+n>Nth number of fibonacci sequence.
     }
-    public static int fibH(int n)
-
     /*As Per classwork*/
     public static ArrayList<Integer> makeAllSums(int n){
     }
