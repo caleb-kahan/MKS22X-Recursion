57eb3591899c98341774ebb9eb4a60156126bff0
Caleb Kahan <ckahan00@stuy.edu>
2019-02-05 16:09:10 -0500

    Added Recursive Part

--- Recursion.java
+++ Recursion.java
@@ -10,6 +10,7 @@ public class Recursion{
       if(n>=0){int hold = n;return sqrt(-1*n/2,tolerance);}
       n*=-1;
       if(Math.abs(n*n-hold)/hold<=tolerance)return n;
+      return sqrt((hold/n+n)/2,tolerance);
 
 
       //I don't want to make a helper function
